ed11d1ccab6bdb6e55784576d2f86a305020e55f
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2021-10-11 21:42:39 +0100

    add state debug messages

--- game.go
+++ game.go
@@ -17,29 +17,36 @@ type Game struct {
 }
 
 func NewGame() (*Game, error) {
+	var game Game
 	cards, err := LoadCards("cards.json")
 	if err != nil {
 		return nil, err
 	}
+	game.cards = cards
 
 	fsm := fsm.NewFSM(
 		"GameStart",
 		fsm.Events{
-			{Name: "GameEnded", Src: []string{"GameStart"}, Dst: "GameOver"},
+			{Name: "GameEnded", Src: []string{"GameStart", "RoundStart", "RoundOver"}, Dst: "GameOver"},
+			{Name: "RoundStarted", Src: []string{"GameStart"}, Dst: "RoundStart"},
+			{Name: "RoundEnded", Src: []string{"RoundStart"}, Dst: "RoundOver"},
 		},
 		fsm.Callbacks{
 			"enter_state": func(e *fsm.Event) {
 				log.Printf("event e: %v", e)
 			},
+			"enter_RoundStart": func(e *fsm.Event) {
+				game.InitDeck()
+			},
 		},
 	)
-	return &Game{
-		fsm:   fsm,
-		cards: cards,
-	}, nil
+	game.fsm = fsm
+
+	return &game, nil
 }
 
 func (g *Game) InitDeck() {
+	log.Println("initializing deck")
 	deck := NewDeck(g.cards)
 	deck.Shuffle(time.Now().UnixNano())
 	g.deck = deck
@@ -48,6 +55,16 @@ func (g *Game) InitDeck() {
 func (g *Game) Update() error {
 	switch g.fsm.Current() {
 	case "GameStart":
+		if ebiten.IsKeyPressed(ebiten.KeyEnter) {
+			g.fsm.Event("RoundStarted")
+		} else if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+			g.fsm.Event("GameEnded")
+		}
+	case "RoundStart":
+		if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+			g.fsm.Event("RoundEnded")
+		}
+	case "RoundOver":
 		g.fsm.Event("GameEnded")
 	case "GameOver":
 		os.Exit(0)
@@ -58,7 +75,18 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	switch g.fsm.Current() {
+	case "GameStart":
+		ebitenutil.DebugPrint(screen, "Game Start")
+	case "RoundStart":
+		ebitenutil.DebugPrint(screen, "Round Start")
+	case "RoundOver":
+		ebitenutil.DebugPrint(screen, "Round Over")
+	case "GameOver":
+		ebitenutil.DebugPrint(screen, "Game Over")
+	default:
+		log.Fatalf("invalid game state: %s", g.fsm.Current())
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
